2c7813145485636afeab725a755c948be3e1a4cf
AliAbbas <aliabbasmaryami50@gmail.com>
2024-02-24 02:12:15 -0800

    for loop

--- script.js
+++ script.js
@@ -216,7 +216,10 @@
 
     // for loop//-----------
 
-    for(let i= 0; i<=20; i++){
+    // for(let i= 0; i<=20; i++){
+    //     console.log(i)
+    // }
+
+    for (let i=1; i<=10; i++){
         console.log(i)
     }
-    
